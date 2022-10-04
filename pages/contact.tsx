99a173f4ef46ea2ca26ea32bdd4750bf88f5f2c4
Valeri Sabev <valchygaming@gmail.com>
2022-10-04 13:31:35 +0200

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Page } from '../components/Layout/Page';
 import { Header } from '../components/Team/styles';
 
@@ -7,6 +8,12 @@ export default function Contact() {
 			<title>Mando | Contact</title>
 			<div className="container mw-100">
 				<Header>Contact Us</Header>
+				<p style={{ display: 'flex', flexDirection: 'column', color: '#fff', fontSize: 22 }}>
+					<span>Send us your inquiry at:</span>
+					<Link href="mailto:mandoapp.contact@gmail.com">
+						<a style={{ color: '#fff' }}>mandoapp.contact@gmail.com</a>
+					</Link>
+				</p>
 			</div>
 		</Page>
 	);
